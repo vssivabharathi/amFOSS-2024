1813755af575075c1768aae0b1300e586c899176
Sivabharathi <vssivabharathi@gmail.com>
2024-08-28 21:17:06 +0530

    Added the icons in the task-05

--- task-05/script.js
+++ task-05/script.js
@@ -1,3 +1,36 @@
+// USING THE FAKEAPI for the product
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('https://fakestoreapi.com/products')
+        .then(response => response.json())
+        .then(products => {
+            const productList = document.getElementById('product-list');
+
+            products.forEach(product => {
+                const productCard = document.createElement('div');
+                productCard.classList.add('product-card');
+
+                productCard.innerHTML = `
+                    <img src="${product.image}" alt="${product.title}">
+                    <div class="price-info">
+                        <span class="old-price">$${(product.price * 1.2).toFixed(2)}</span>
+                        <span class="new-price">$${product.price}</span>
+                    </div>
+                    <div class="actions">
+                        <button class="wishlist"><span class="material-symbols-outlined">
+                            favorite
+                            </span></button>
+                        <button class="add-to-cart"><span class="material-symbols-outlined">
+                            shopping_cart
+                            </span></button>
+                    </div>
+                `;
+
+                productList.appendChild(productCard);
+            });
+        })
+        .catch(error => console.error('Error fetching products:', error));
+});
+
 //fix the errors and complete the code.
 
 const terminalOutput = document.querySelector('.terminal-output');
