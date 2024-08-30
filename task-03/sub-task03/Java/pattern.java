6efebd31b77f490bf60c499d924d4a42fd1b1b36
Sivabharathi <vssivabharathi@gmail.com>
2024-08-30 20:31:35 +0530

    Java programming in pattern finding  in the Sub-task-03, in task-03

---
+++ task-03/sub-task03/Java/pattern.java
@@ -0,0 +1,25 @@
+public class pattern {
+    public static void main(String[] args) {
+        int rows = 3;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = rows - 2; i >= 0; i--) {
+            for (int j = 0; j < rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
